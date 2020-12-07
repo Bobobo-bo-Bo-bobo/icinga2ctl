513a1704367a627e2c936fe4e040e7c61f06b1d7
Andreas Maus <maus@badphish.ypbind.de>
2020-12-07 17:43:51 +0100

    Use clap instead of getopts because clap support subcommands with own options

--- src/main.rs
+++ src/main.rs
@@ -5,62 +5,65 @@ mod configuration;
 mod constants;
 mod usage;
 
-use getopts::Options;
+use clap::{App, Arg, SubCommand};
 use std::env;
 use std::process;
 
 fn main() {
-    let argv: Vec<String> = env::args().collect();
-    let mut options = Options::new();
+    let mut config_file = String::new();
 
-    options.optopt("c", "config", "Configuration file", "config_file");
-    options.optflag("h", "help", "Show help text");
-    options.optflag("v", "version", "Show version");
+    let options = App::new(constants::NAME)
+        .version(constants::VERSION)
+        .about("Interact with Icinga 2 instance")
+        .args(&[
+            Arg::with_name("config_file")
+                .short("c")
+                .long("config")
+                .takes_value(true)
+                .help("Configuration file"),
+            Arg::with_name("help")
+                .short("h")
+                .long("help")
+                .help("Show help text"),
+            Arg::with_name("version")
+                .short("v")
+                .long("version")
+                .help("Show version information"),
+        ])
+        .get_matches();
 
-    let opt = match options.parse(&argv[1..]) {
-        Ok(v) => v,
-        Err(e) => {
-            eprintln!("Error: Can't parse command line arguments: {}", e);
-            process::exit(1);
-        }
-    };
-
-    if opt.opt_present("h") {
+    if options.is_present("help") {
         usage::show_usage();
         process::exit(0);
     }
 
-    if opt.opt_present("v") {
+    if options.is_present("version") {
         usage::show_version();
-        process::exit(0);
-    }
-
-    let config_file = match opt.opt_str("c") {
-        Some(v) => v,
-        None => {
-            let v = match configuration::get_default_user_config_file() {
-                Ok(v) => v,
-                Err(e) => {
-                    eprintln!("{}", e);
-                    process::exit(1);
-                }
-            };
-            v
-        }
-    };
-    let config = match configuration::get_configuration(config_file.as_str()) {
-        Ok(v) => { v },
-        Err(e) => {
-            eprintln!("Error: Can't parse configuration file {}: {}", config_file, e);
-            process::exit(1);
-        },
-    };
-
-    if opt.free.is_empty() {
-        eprintln!("Error: Missing command");
-        usage::show_usage();
         process::exit(1);
     }
 
-    println!("> {:#?}", opt.free);
+    if let Some(v) = options.value_of("config_file") {
+        config_file = v.to_string();
+    }
+
+    if config_file.is_empty() {
+        config_file = match configuration::get_default_user_config_file() {
+            Ok(v) => v,
+            Err(e) => {
+                eprintln!("{}", e);
+                process::exit(1);
+            }
+        };
+    }
+
+    let config = match configuration::get_configuration(config_file.as_str()) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!(
+                "Error: Can't parse configuration file {}: {}",
+                config_file, e
+            );
+            process::exit(1);
+        }
+    };
 }
