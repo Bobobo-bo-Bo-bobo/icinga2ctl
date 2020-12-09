9f0962e0c067b4392a9b0b72feaec9feea42fb81
Andreas Maus <maus@badphish.ypbind.de>
2020-12-09 09:31:13 +0100

    status: Allow for filtering of multiple states

--- src/command.rs
+++ src/command.rs
@@ -6,6 +6,7 @@ use crate::usage;
 use crate::util;
 
 use http::StatusCode;
+use std::collections::HashMap;
 use std::error::Error;
 
 pub fn status(
@@ -16,7 +17,7 @@ pub fn status(
     let mut attrs: &str = "";
     let mut filter = String::new();
     let mut color = true;
-    let mut state_filter = ">=0";
+    let mut state_map = HashMap::new();
 
     if opt.is_present("help") {
         usage::show_usage_status();
@@ -24,16 +25,16 @@ pub fn status(
     }
 
     if opt.is_present("ok") {
-        state_filter = "==0";
+        state_map.insert("== 0".to_string(), String::new());
     }
     if opt.is_present("warning") {
-        state_filter = "==1";
+        state_map.insert("==1".to_string(), String::new());
     }
     if opt.is_present("critical") {
-        state_filter = "==2";
+        state_map.insert("==2".to_string(), String::new());
     }
     if opt.is_present("unknown") {
-        state_filter = "== 3";
+        state_map.insert("==3".to_string(), String::new());
     }
 
     if opt.is_present("no-color") {
@@ -55,8 +56,9 @@ pub fn status(
     if !hosts.is_empty() && services.is_empty() {
         // Show host status for hosts
         obj = constants::ICINGA2_OBJ_HOST;
+        let state_filter = build_state_filter("host", &state_map);
         filter = format!(
-            "{{\"filter\":\"match(\\\"{}\\\", host.name) && host.state {}\"}}",
+            "{{\"filter\":\"match(\\\"{}\\\", host.name) && {}\"}}",
             hosts, state_filter
         );
         attrs = "attrs=name&attrs=display_name&attrs=last_check_result&attrs=state";
@@ -65,8 +67,9 @@ pub fn status(
     if hosts.is_empty() && !services.is_empty() {
         // Show services for all hosts
         obj = constants::ICINGA2_OBJ_SERVICE;
+        let state_filter = build_state_filter("service", &state_map);
         filter = format!(
-            "{{\"filter\":\"match(\\\"{}\\\", service.name) && service.state {}\"}}",
+            "{{\"filter\":\"match(\\\"{}\\\", service.name) && {}\"}}",
             services, state_filter
         );
         attrs = "attrs=display_name&attrs=host_name&attrs=last_check_result&attrs=state";
@@ -75,8 +78,9 @@ pub fn status(
     if !hosts.is_empty() && !services.is_empty() {
         // Show services for hosts
         obj = constants::ICINGA2_OBJ_SERVICE;
+        let state_filter = build_state_filter("service", &state_map);
         filter = format!(
-            "{{\"filter\":\"match(\\\"{}\\\", host.name) && match(\\\"{}\\\", service.name) && service.state {}\"}}",
+            "{{\"filter\":\"match(\\\"{}\\\", host.name) && match(\\\"{}\\\", service.name) && {}\"}}",
             hosts, services, state_filter
         );
         attrs = "attrs=display_name&attrs=host_name&attrs=last_check_result&attrs=state";
@@ -148,4 +152,15 @@ pub fn status(
         }
     }
     Ok(())
+}
+
+fn build_state_filter(n: &str, m: &HashMap<String, String>) -> String {
+    if !m.is_empty() {
+        let mut v = Vec::new();
+        for key in m.keys() {
+            v.push(format!("{}.state {}", n, key));
+        }
+        return format!("({})", v.join(" || "));
+    }
+    format!("{}.state >= 0", n)
 }
