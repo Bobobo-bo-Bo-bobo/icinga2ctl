7222f1c32a19a2f1db4049b1f27d8b61191c5f33
Andreas Maus <maus@badphish.ypbind.de>
2020-12-11 14:13:56 +0100

    Add help text for downtimes

--- src/usage.rs
+++ src/usage.rs
@@ -77,6 +77,9 @@ pub fn show_usage_add_ack() {
     println!(
         "    add-ack - add acknowledgement
 
+        -C <comment>                Set acknowledgement comment
+        --comment=<comment>         This option is mandatory.
+
         -H <host_object>            Show status of host <host_object>
         --host=<host_object>
 
@@ -88,9 +91,6 @@ pub fn show_usage_add_ack() {
 
         -a <author>                 Set author of acknowledgement
         --author=<author>           Default: current user
-
-        -C <comment>                Set acknowledgement comment
-        --comment=<comment>         This option is mandatory.
 
         -c                          Acknowledge host/services with CRITICAL state
         --critical
@@ -140,6 +140,63 @@ pub fn show_usage_del_ack() {
 
         -w                          Acknowledge host/services with WARNING state
         --warning
+"
+    );
+}
+
+pub fn show_usage_add_downtime() {
+    show_version();
+    println!(
+        "    add-downtime - schedule a downtime
+
+        -A                          If host downtime is added, add downtime for all services too.
+        --all-services              Only valid for host downtimes
+
+        -C <comment>                Set downtime comment.
+        --comment=<comment>         This option is mandatory.
+
+        -H <host_object>            Show status of host <host_object>
+        --host=<host_object>
+
+        -S <service_object>         Show status of service <service_object>
+        --service=<service_object>
+
+        -a <author>                 Set author of downtime
+        --author=<author>           Default: current user
+
+        -c <childopts>              Schedule child downtime.
+        --child=<childopts>         <childopts> can be one of:
+                                        no         - don't schedule child downtimes
+                                                     This is the default behavior.
+                                        trigger    - Add child downtimes triggered by the downtime to be added
+                                        no-trigger - Add non-triggered child downtimes
+                                    See downtime documentation for futher information.
+
+        -d <sec>                    Set downtime duration for flexible downtime
+        --duration=<sec>            This option is mandatory for flexible downtimes.
+                                    See downtime documentation for further information.
+
+        -e <time>                   Set end time of downtime.
+        --end=<time>                <time> must be in the format as specified in RFC3339,
+                                    e.g. 2010-11-09T12:54:11Z for UTC or 2020-11-09T23:11:12+02:00 for
+                                    a time with time zone.
+                                    This option is mandatory.
+
+        -f                          Add fixed downtime instead of a flexible downtime.
+        --fixed                     See downtime documentation for further information.
+
+        -h                          Show this text
+        --help
+
+        -s <time>                   Set start time of downtime
+        --start=<time>              <time> must be in the format as specified in RFC3339,
+                                    e.g. 2010-11-09T12:54:11Z for UTC or 2020-11-09T23:11:12+02:00 for
+                                    a time with time zone.
+                                    This option is mandatory.
+
+        -t <trigger>                Add trigger for triggered downtime.
+        --trigger=<trigger>         See downtime documentation for futher information.
+
 "
     );
 }
